1b80be8cfe042a90f3d2c41e082c38e6b449dedf
666satan <68298527+qqqqsatan@users.noreply.github.com>
2021-07-17 14:34:20 +0700

    Menambahkan commit file

---
+++ WEBGIS/data/KESEHATAN_PT_25K_2.js
@@ -0,0 +1,12 @@
+var json_KESEHATAN_PT_25K_2 = {
+"type": "FeatureCollection",
+"name": "KESEHATAN_PT_25K_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAMOBJ": "Fasilitas Kesehatan Lainnya", "LUAS": 0.0, "REMARK": "Fasilitas Kesehatan Lainnya", "TIPSHT": "999", "FCODE": null, "SRS_ID": null, "LCODE": "GI0020", "METADATA": null, "FSKADD": null }, "geometry": { "type": "Point", "coordinates": [ 110.882820320000064, -6.453395, 0.0 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": "Fasilitas Kesehatan Lainnya", "LUAS": 0.0, "REMARK": "Fasilitas Kesehatan Lainnya", "TIPSHT": "999", "FCODE": null, "SRS_ID": null, "LCODE": "GI0020", "METADATA": null, "FSKADD": null }, "geometry": { "type": "Point", "coordinates": [ 110.891829076000079, -6.486092923999934, 0.0 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": "Fasilitas Kesehatan Lainnya", "LUAS": 0.0, "REMARK": "Fasilitas Kesehatan Lainnya", "TIPSHT": "999", "FCODE": null, "SRS_ID": null, "LCODE": "GI0020", "METADATA": null, "FSKADD": null }, "geometry": { "type": "Point", "coordinates": [ 110.912973643000043, -6.408436631999962, 0.0 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": "Fasilitas Kesehatan Lainnya", "LUAS": 0.0, "REMARK": "Fasilitas Kesehatan Lainnya", "TIPSHT": "999", "FCODE": null, "SRS_ID": null, "LCODE": "GI0020", "METADATA": null, "FSKADD": null }, "geometry": { "type": "Point", "coordinates": [ 110.895779567000034, -6.595395398999926, 0.0 ] } },
+{ "type": "Feature", "properties": { "NAMOBJ": "Fasilitas Kesehatan Lainnya", "LUAS": 0.0, "REMARK": "Fasilitas Kesehatan Lainnya", "TIPSHT": "999", "FCODE": null, "SRS_ID": null, "LCODE": "GI0020", "METADATA": null, "FSKADD": null }, "geometry": { "type": "Point", "coordinates": [ 110.906601099000056, -6.501921809999942, 0.0 ] } }
+]
+}
